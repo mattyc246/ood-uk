f741dae3c4228135797f0d17925310822b077e4c
Matthew Cross <mattyc246@gmail.com>
2020-01-21 14:06:27 +0800

    added page header for homepage with search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Container, Content, Text } from "native-base";
+import {
+  Container,
+  Content,
+  Text,
+  View,
+  Item,
+  Input,
+  Icon,
+  H1
+} from "native-base";
 import MainHeader from "../components/MainHeader";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <Container style={styles.container}>
-      <MainHeader height={100} showBack={false} showCart={true} title="Home" />
+      <MainHeader height={100} showBack={false} showCart={true} title="" />
       <Content contentContainerStyle={styles.content}>
+        <View style={styles.pageHeader}>
+          <H1 style={styles.textCenter}>OOD</H1>
+          <Text style={styles.textCenter}>Browse, compare, buy</Text>
+          <Item style={styles.searchBar}>
+            <Input placeholder="Search Products" />
+            <Icon active name="search" />
+          </Item>
+        </View>
         <Text>Home</Text>
       </Content>
     </Container>
@@ -19,9 +36,25 @@ const styles = {
   },
   content: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white"
+  },
+  pageHeader: {
+    height: 160,
+    width: "100%",
+    backgroundColor: "rgb(145, 208, 91)",
+    padding: 5,
+    alignItems: "center"
+  },
+  textCenter: {
+    textAlign: "center",
+    marginBottom: 15
+  },
+  searchBar: {
+    backgroundColor: "white",
+    width: "90%",
+    marginTop: 10,
+    borderRadius: 10
   }
 };
 
