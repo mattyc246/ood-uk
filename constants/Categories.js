d5d5adad1d326256ea1d7f4714cc6fd0365f2767
Matthew Cross <mattyc246@gmail.com>
2020-01-29 12:14:03 +0800

    basic layout for categories page done

---
+++ constants/Categories.js
@@ -0,0 +1,59 @@
+const productCategories = [
+  {
+    name: "Fresh Food",
+    params: "freshfood",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Bakery",
+    params: "bakery",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Frozen Food",
+    params: "forezenfood",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Food Cupboard",
+    params: "foodcupboard",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Drinks",
+    params: "drinks",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Baby",
+    params: "baby",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Health & Beauty",
+    params: "healthbeauty",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Pets",
+    params: "pets",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Household",
+    params: "household",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Home & Ents",
+    params: "homeents",
+    imgUrl: "https://picsum.photos/150/150"
+  },
+  {
+    name: "Inspiration & Events",
+    params: "inspirationevents",
+    imgUrl: "https://picsum.photos/150/150"
+  }
+];
+
+export { productCategories };
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { View, Text, Container, Content } from "native-base";
+import {
+  View,
+  Text,
+  Container,
+  Content,
+  Thumbnail,
+  Card,
+  H1,
+  Button
+} from "native-base";
 import MainHeader from "../components/MainHeader";
+import { productCategories } from "../constants/Categories";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
   return (
     <Container style={styles.container}>
       <MainHeader
         height={100}
         showBack={false}
         showCart={true}
-        title="Products"
+        title="Categories"
       />
       <Content contentContainerStyle={styles.content}>
-        <Text>Hello World</Text>
+        {productCategories.map((category, index) => {
+          return (
+            <Button bordered dark style={styles.invisibleButton}>
+              <Thumbnail large source={{ uri: category.imgUrl }} />
+              <Text style={styles.textCenter}>{category.name}</Text>
+            </Button>
+          );
+        })}
       </Content>
     </Container>
   );
@@ -26,10 +44,23 @@ const styles = {
     width: "100%",
     backgroundColor: "white",
     padding: 10,
-    flexDirection: "row",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    alignItems: "center"
+    flexGrow: 1
+  },
+  card: {
+    width: "30%",
+    height: "20%",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  invisibleButton: {
+    height: 125,
+    margin: 10,
+    padding: 10,
+    justifyContent: "space-between"
+  },
+  textCenter: {
+    textAlign: "center"
   }
 };
 
